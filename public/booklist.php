5a19b93fb566161de11ee069080625e81cc220cd
Maryna <marynahaidashevska@gmail.com>
2019-06-12 21:56:42 -0500

    Updated showModal function added

---
+++ public/booklist.php
@@ -0,0 +1,19 @@
+<h2 class="title">Book List</h2>
+
+<table class="table">
+	<tr>
+		<th>Title</th>
+		<th>Author</th>
+		<th>Genre</th>
+		<th>Year</th>
+	</tr>
+
+	<?php foreach($result as $row) : ?>
+		<tr>
+			<td><?=htmlspecialchars($row['title'])?></a></td>
+			<td><?=htmlspecialchars($row['author'])?></td>
+			<td><?=htmlspecialchars($row['genre'])?></td>
+			<td><?=htmlspecialchars($row['year_published'])?></td>
+		</tr>
+    <?php endforeach; ?>
+</table>
---
+++ public/server.php
@@ -0,0 +1,44 @@
+<?php
+
+$dbh = new PDO('mysql:host=localhost;dbname=booksite', 'root', '' );
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if(isset($_GET['book_id'])){
+	$query = "SELECT book.* ,
+	author.name as author,
+	publisher.name as publisher,
+	format.name as format,
+	genre.name as genre 
+	FROM book
+	JOIN author USING (author_id)
+	JOIN publisher USING (publisher_id)
+	JOIN format USING (format_id)
+	JOIN genre USING (genre_id)
+    WHERE book_id = :book_id";
+	$params = array(':book_id' =>(int) $_GET['book_id']);
+	$stmt = $dbh->prepare($query);
+    $stmt->execute($params);
+
+    $book = $stmt->fetch(PDO::FETCH_ASSOC);
+    include 'detail.php';
+    
+} else{
+	$query = "SELECT book.*,
+	author.name as author,
+	publisher.name as publisher,
+	format.name as format,
+	genre.name as genre 
+	FROM book
+	JOIN author USING (author_id)
+	JOIN publisher USING (publisher_id)
+	JOIN format USING (format_id)
+	JOIN genre USING (genre_id)";
+
+	$params = array();
+	$stmt = $dbh->prepare($query);
+    $stmt->execute($params);
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    include 'booklist.php';
+}
+
